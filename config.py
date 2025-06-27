a6c5a13e43fc8cb02fcaf05baad984dbccb9fafa
cookies2002 <cookies2002boy@gmail.com>
2025-06-27 23:18:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+ALLOWED_USERS = [int(os.getenv("OWNER_ID")), 123456789, 987654321]
