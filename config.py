f20c1c6975b502a1be6d37ffe582214710856fb0
cookies2002 <cookies2002boy@gmail.com>
2025-06-27 23:56:21 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ALLOWED_USERS = [int(os.getenv("OWNER_ID")), 123456789, 987654321]
